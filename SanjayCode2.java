23b3e89305e2741d2144dbf22f46ade9bab2bab7
bhoomikadiwan <bhoomika.diwan@gmail.com>
2021-07-22 20:34:04 +0200

    Update SanjayCode2.java
    Save new changes to string

--- SanjayCode2.java
+++ SanjayCode2.java
@@ -7,7 +7,7 @@ import java.util.*;
 class ReverseString {
     public static void main(String[] args)
     {
-        String input = "GeeksforGeeks";
+        String input = "GeeksforGeeks-SanjayBhardan";
  
         // getBytes() method to convert string
         // into bytes[].
